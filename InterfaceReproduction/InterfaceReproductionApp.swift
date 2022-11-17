d04812b3a89fbb4d34e44105e6c21b265e863731
Morgan <Mharrisstoertz@gmail.com>
2022-11-17 09:00:06 -0500

    Initial Commit

---
+++ InterfaceReproduction/InterfaceReproductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceReproductionApp.swift
+//  InterfaceReproduction
+//
+//  Created by Morgan Harris-Stoertz on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceReproductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
